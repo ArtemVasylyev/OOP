d03c7321766e68e953d5e2ef5633bef60c40f24a
ArtemVasylyev <av3350899@gmail.com>
2026-04-23 17:18:49 +0200

    Lab_8

---
+++ Lab_8/Lab_8/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Input;
+using Microsoft.Win32;
+using System.IO;
+
+namespace Lab08_WPF_Editor
+{
+    public partial class MainWindow : Window
+    {
+        private bool isEn = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmbFontSize.ItemsSource = new double[] { 8, 10, 12, 14, 16, 18, 20, 24, 32, 48 };
+
+            this.CommandBindings.Add(new CommandBinding(ApplicationCommands.Open, Open_Executed));
+            this.CommandBindings.Add(new CommandBinding(ApplicationCommands.Save, Save_Executed));
+        }
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            cmbFontFamily.SelectedItem = temp;
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            cmbFontSize.Text = temp.ToString();
+        }
+
+        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFontFamily.SelectedItem != null)
+                rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+        }
+
+        private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog { Filter = "Rich Text Format (*.rtf)|*.rtf" };
+            if (dlg.ShowDialog() == true)
+            {
+                using (FileStream fs = new FileStream(dlg.FileName, FileMode.Open))
+                {
+                    TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                    range.Load(fs, DataFormats.Rtf);
+                }
+            }
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog { Filter = "Rich Text Format (*.rtf)|*.rtf" };
+            if (dlg.ShowDialog() == true)
+            {
+                using (FileStream fs = new FileStream(dlg.FileName, FileMode.Create))
+                {
+                    TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                    range.Save(fs, DataFormats.Rtf);
+                }
+            }
+        }
+
+        private void NewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+        }
+
+        private void InsertImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog { Filter = "Images|*.png;*.jpg;*.bmp" };
+            if (dlg.ShowDialog() == true)
+            {
+                var bitmap = new BitmapImage(new Uri(dlg.FileName));
+                var image = new Image { Source = bitmap, Width = 200 };
+                new InlineUIContainer(image, rtbEditor.CaretPosition);
+            }
+        }
+
+        private void ChangeLang_Click(object sender, RoutedEventArgs e)
+        {
+            isEn = !isEn;
+            btnImage.Content = isEn ? "🖼 Image" : "🖼 Картинка";
+            btnLang.Content = isEn ? "🌐 Lang" : "🌐 Мова";
+            btnNew.Content = isEn ? "📄 New Window" : "📄 Нове вікно";
+            this.Title = isEn ? "My WPF Editor" : "Мій WPF Редактор";
+        }
+    }
+}
