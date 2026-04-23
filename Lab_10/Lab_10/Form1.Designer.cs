ec0dabcaaabcd2e130fbfac7c01180d9cdef6511
ArtemVasylyev <av3350899@gmail.com>
2026-04-23 20:39:23 +0200

    Lab_10

---
+++ Lab_10/Lab_10/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Lab_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnStart = new Button();
+            btnStop = new Button();
+            listBoxLog = new ListBox();
+            SuspendLayout();
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(294, 574);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(219, 55);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "Запустити всі потоки";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(856, 574);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(213, 55);
+            btnStop.TabIndex = 1;
+            btnStop.Text = "Зупинити всі потоки";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // listBoxLog
+            // 
+            listBoxLog.FormattingEnabled = true;
+            listBoxLog.Location = new Point(294, 12);
+            listBoxLog.Name = "listBoxLog";
+            listBoxLog.Size = new Size(775, 529);
+            listBoxLog.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1407, 676);
+            Controls.Add(listBoxLog);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnStart;
+        private Button btnStop;
+        private ListBox listBoxLog;
+    }
+}
