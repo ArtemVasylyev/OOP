7058f0373d60b18af80cf105973feaba4b9bbbf0
ArtemVasylyev <av3350899@gmail.com>
2026-04-26 14:16:47 +0200

    Lab_13

---
+++ Lab_13/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Lab_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxDrives = new System.Windows.Forms.ComboBox();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.rtbProperties = new System.Windows.Forms.RichTextBox();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.listBoxFolders = new System.Windows.Forms.ListBox();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxDrives
+            // 
+            this.comboBoxDrives.FormattingEnabled = true;
+            this.comboBoxDrives.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxDrives.Name = "comboBoxDrives";
+            this.comboBoxDrives.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxDrives.TabIndex = 0;
+            this.comboBoxDrives.SelectedIndexChanged += new System.EventHandler(this.comboBoxDrives_SelectedIndexChanged);
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Location = new System.Drawing.Point(170, 14);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(424, 26);
+            this.txtFilter.TabIndex = 1;
+            // 
+            // rtbProperties
+            // 
+            this.rtbProperties.Location = new System.Drawing.Point(411, 398);
+            this.rtbProperties.Name = "rtbProperties";
+            this.rtbProperties.Size = new System.Drawing.Size(524, 423);
+            this.rtbProperties.TabIndex = 2;
+            this.rtbProperties.Text = "";
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(12, 559);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(269, 45);
+            this.btnFilter.TabIndex = 3;
+            this.btnFilter.Text = "Застосувати фільтр";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // listBoxFolders
+            // 
+            this.listBoxFolders.FormattingEnabled = true;
+            this.listBoxFolders.ItemHeight = 20;
+            this.listBoxFolders.Location = new System.Drawing.Point(12, 70);
+            this.listBoxFolders.Name = "listBoxFolders";
+            this.listBoxFolders.Size = new System.Drawing.Size(350, 304);
+            this.listBoxFolders.TabIndex = 4;
+            this.listBoxFolders.SelectedIndexChanged += new System.EventHandler(this.listBoxFolders_SelectedIndexChanged);
+            this.listBoxFolders.DoubleClick += new System.EventHandler(this.listBoxFolders_DoubleClick);
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.ItemHeight = 20;
+            this.listBoxFiles.Location = new System.Drawing.Point(368, 70);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(382, 304);
+            this.listBoxFiles.TabIndex = 5;
+            this.listBoxFiles.SelectedIndexChanged += new System.EventHandler(this.listBoxFiles_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(812, 125);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(249, 206);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 865);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.listBoxFolders);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.rtbProperties);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.comboBoxDrives);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxDrives;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.RichTextBox rtbProperties;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.ListBox listBoxFolders;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Lab_13/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab_13
+{
+    public partial class Form1 : Form
+    {
+        private string currentPath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDrives();
+        }
+
+        private void LoadDrives()
+        {
+            comboBoxDrives.Items.Clear();
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    comboBoxDrives.Items.Add(drive.Name);
+                }
+            }
+            if (comboBoxDrives.Items.Count > 0)
+            {
+                comboBoxDrives.SelectedIndex = 0;
+            }
+        }
+
+        private void comboBoxDrives_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentPath = comboBoxDrives.SelectedItem.ToString();
+            LoadDirectory(currentPath);
+            ShowDriveProperties(currentPath);
+        }
+
+        private void LoadDirectory(string path, string filter = "*")
+        {
+            try
+            {
+                listBoxFolders.Items.Clear();
+                listBoxFiles.Items.Clear();
+
+                if (path.Length > 3)
+                {
+                    listBoxFolders.Items.Add(".. [НАЗАД]");
+                }
+
+                DirectoryInfo dir = new DirectoryInfo(path);
+
+                foreach (DirectoryInfo d in dir.GetDirectories(filter))
+                {
+                    listBoxFolders.Items.Add(d.Name);
+                }
+
+                foreach (FileInfo f in dir.GetFiles(filter))
+                {
+                    listBoxFiles.Items.Add(f.Name);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка доступу: " + ex.Message);
+            }
+        }
+
+        private void listBoxFolders_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBoxFolders.SelectedItem == null) return;
+
+            string selected = listBoxFolders.SelectedItem.ToString();
+
+            if (selected == ".. [НАЗАД]")
+            {
+                currentPath = Directory.GetParent(currentPath).FullName;
+            }
+            else
+            {
+                currentPath = Path.Combine(currentPath, selected);
+            }
+
+            LoadDirectory(currentPath);
+        }
+
+        private void listBoxFolders_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxFolders.SelectedItem == null || listBoxFolders.SelectedItem.ToString() == ".. [НАЗАД]") return;
+
+            string folderPath = Path.Combine(currentPath, listBoxFolders.SelectedItem.ToString());
+            DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
+
+            rtbProperties.Text = "=== ВЛАСТИВОСТІ КАТАЛОГУ ===\n";
+            rtbProperties.Text += $"Назва: {dirInfo.Name}\n";
+            rtbProperties.Text += $"Шлях: {dirInfo.FullName}\n";
+            rtbProperties.Text += $"Створено: {dirInfo.CreationTime}\n";
+
+            ShowSecurityAttributes(folderPath, false);
+        }
+
+        private void listBoxFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxFiles.SelectedItem == null) return;
+
+            string filePath = Path.Combine(currentPath, listBoxFiles.SelectedItem.ToString());
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            rtbProperties.Text = "=== ВЛАСТИВОСТІ ФАЙЛУ ===\n";
+            rtbProperties.Text += $"Назва: {fileInfo.Name}\n";
+            rtbProperties.Text += $"Розмір: {fileInfo.Length} байт\n";
+            rtbProperties.Text += $"Створено: {fileInfo.CreationTime}\n";
+
+            ShowSecurityAttributes(filePath, true);
+
+            pictureBox1.Image = null;
+            string ext = fileInfo.Extension.ToLower();
+
+            try
+            {
+                if (ext == ".txt" || ext == ".log" || ext == ".cs")
+                {
+                    rtbProperties.Text += "\n=== ВМІСТ ===\n" + File.ReadAllText(filePath);
+                }
+                else if (ext == ".jpg" || ext == ".png" || ext == ".bmp")
+                {
+                    pictureBox1.Image = Image.FromFile(filePath);
+                }
+            }
+            catch { }
+        }
+
+        private void ShowDriveProperties(string driveName)
+        {
+            DriveInfo drive = new DriveInfo(driveName);
+            rtbProperties.Text = "=== ВЛАСТИВОСТІ ДИСКА ===\n";
+            rtbProperties.Text += $"Назва: {drive.Name}\n";
+            rtbProperties.Text += $"Тип: {drive.DriveType}\n";
+            rtbProperties.Text += $"Формат: {drive.DriveFormat}\n";
+            rtbProperties.Text += $"Вільний простір: {drive.TotalFreeSpace / (1024 * 1024 * 1024)} ГБ\n";
+        }
+
+        private void ShowSecurityAttributes(string path, bool isFile)
+        {
+            try
+            {
+                rtbProperties.Text += "\n=== ДОСТУП ===\n";
+                AuthorizationRuleCollection rules;
+
+                if (isFile)
+                {
+                    rules = File.GetAccessControl(path).GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+                }
+                else
+                {
+                    rules = Directory.GetAccessControl(path).GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+                }
+
+                foreach (FileSystemAccessRule rule in rules)
+                {
+                    rtbProperties.Text += $"{rule.IdentityReference}: {rule.FileSystemRights}\n";
+                }
+            }
+            catch { }
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            string filter = string.IsNullOrWhiteSpace(txtFilter.Text) ? "*" : txtFilter.Text;
+            LoadDirectory(currentPath, filter);
+        }
+    }
+}
